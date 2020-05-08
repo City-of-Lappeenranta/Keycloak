d1d83225e82f8562d78d23b8aa78f0913b4727ad
Andrey Aleksandrov <andrey.aleksandrov@visma.com>
2020-05-08 09:21:01 +0300

    Update default docker volume mount point, update localization

--- start_docker.sh
+++ start_docker.sh
@@ -1,1 +1,1 @@
-docker run -p 8080:8080 --name keycloak_tunnistamo -d --env-file .env -v /tmp/keycloak/data:/opt/jboss/keycloak/standalone/data img_keycloak_tunnistamo 
+docker run -p 8080:8080 --name keycloak_tunnistamo -d --env-file .env -v /var/opt/keycloak:/opt/jboss/keycloak/standalone/data img_keycloak_tunnistamo 
